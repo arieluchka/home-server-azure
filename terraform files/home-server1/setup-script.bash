f72a880116b56e6bed6a147080b87b499f6b7f6f
Ariel Agranovich <ariel.agra@gmail.com>
2023-10-30 11:53:47 +0200

    added users and groups. chmod chown

--- terraform files/home-server1/setup-script.bash
+++ terraform files/home-server1/setup-script.bash
@@ -5,9 +5,21 @@ configpaths=/home-server/configs
 mediapaths=/home-server/media
 startupcomposes=/home-server/composes
 
-$configpaths
-$mediapaths
+# MOUNT CLOUD SAVE
+################
+sudo mkdir /mnt/home-server-save
+if [ ! -d "/etc/smbcredentials" ]; then
+sudo mkdir /etc/smbcredentials
+fi
+if [ ! -f "/etc/smbcredentials/homeserverarieluchka.cred" ]; then
+    sudo bash -c 'echo "username=homeserverarieluchka" >> /etc/smbcredentials/homeserverarieluchka.cred'
+    sudo bash -c 'echo "password=M1o0Gxu4mnA8rD+jQiQNQ1szL0w6FOyzgWcGU/K9p2WJ2BBitvG18oBmJ1QS+RlC9Y3Wg+YoDsJR+AStgcU3ow==" >> /etc/smbcredentials/homeserverarieluchka.cred'
+fi
+sudo chmod 600 /etc/smbcredentials/homeserverarieluchka.cred
 
+sudo bash -c 'echo "//homeserverarieluchka.file.core.windows.net/home-server /mnt/home-server-save cifs nofail,credentials=/etc/smbcredentials/homeserverarieluchka.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30" >> /etc/fstab'
+sudo mount -t cifs //homeserverarieluchka.file.core.windows.net/home-server /mnt/home-server-save -o credentials=/etc/smbcredentials/homeserverarieluchka.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30
+################
 
 
 sudo apt-get update
@@ -28,6 +40,20 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 # to download github files
 sudo apt-get install subversion -y
 
+# create users and groups
+sudo groupadd -g 1200 media-server
+sudo useradd -U -u 1201 sonarr
+sudo useradd -U -u 1202 radarr
+sudo useradd -U -u 1203 qbit
+sudo useradd -U -u 1204 jellyfin
+sudo useradd -U -u 1205 jellyseerr
+sudo useradd -U -u 1206 prowlarr
+
+#add services to same group
+gpasswd -M sonarr,radarr,qbit,jellyfin,jellyseerr,prowlarr media-server
+
+
+
 # create folder structure for config saves
 sudo mkdir /home-server
 sudo mkdir $configpaths
@@ -37,48 +63,83 @@ sudo mkdir $mediapaths
 sudo svn export https://github.com/arieluchka/home-server-azure/trunk/composes $startupcomposes
 
 
+
 # JELLYFIN
 # jellyfin folder creation
-
 sudo mkdir $configpaths/jellyfin
 sudo mkdir $configpaths/jellyfin/config
 sudo mkdir $configpaths/jellyfin/cache
+sudo chown -R jellyfin:jellyfin $configpaths/jellyfin
+sudo chmod -R 770 $configpaths/jellyfin
+
 sudo mkdir $mediapaths/jellyfin/
 sudo mkdir $mediapaths/jellyfin/media
 sudo mkdir $mediapaths/jellyfin/media/shows
 sudo mkdir $mediapaths/jellyfin/media/movies
+sudo chown -R jellyfin:media-server $mediapaths/jellyfin/
+sudo chmod -R 774 $mediapaths/jellyfin/
 
-# compose up
 sudo docker compose -f $startupcomposes/jellyfin/docker-compose.yaml up -d
+
 
 # QBITTORRENT
 sudo mkdir $mediapaths/qbittorrent
 sudo mkdir $mediapaths/qbittorrent/downloads
+sudo chown -R qbit:media-server $mediapaths/qbittorrent/
+sudo chmod -R 774 $mediapaths/qbittorrent/
 
 sudo mkdir $configpaths/qbittorrent
 sudo mkdir $configpaths/qbittorrent/configs
+sudo chown -R qbit:qbit $configpaths/qbittorrent
+sudo chmod -R 770 $configpaths/qbittorrent
 
-# compose up
 sudo docker compose -f $startupcomposes/qbittorrent/docker-compose.yaml up -d
 
+
 # SONARR
-sudo mkdir /home-server/configs/sonarr
-sudo mkdir /home-server/configs/sonarr/config
+sudo mkdir $configpaths/sonarr
+sudo mkdir $configpaths/sonarr/config
+sudo chown -R sonarr:sonarr $configpaths/sonarr
+sudo chmod -R 770 $configpaths/sonarr
 
 sudo docker compose -f $startupcomposes/sonarr/docker-compose.yaml up -d
 
+# RADARR
+sudo mkdir $configpaths/radarr
+sudo mkdir $configpaths/radarr/config
+sudo chown -R radarr:radarr $configpaths/radarr
+sudo chmod -R 770 $configpaths/radarr
+
+sudo docker compose -f $startupcomposes/radarr/docker-compose.yaml up -d
+
 
 # PROWLARR
+sudo mkdir $configpaths/prowlarr
+sudo chown -R prowlarr:prowlarr $configpaths/prowlarr
+sudo chmod -R 770 $configpaths/prowlarr
 
-sudo mkdir /home-server/configs/prowlarr
-
-# compose up
 sudo docker compose -f $startupcomposes/prowlarr/docker-compose.yaml up -d
 
-# JELLYSEER
 
+# JELLYSEER
 sudo mkdir $configpaths/jellyseer
+sudo chown -R jellyseer:jellyseer $configpaths/jellyseer
+sudo chmod -R 770 $configpaths/jellyseer
 
 sudo docker compose -f $startupcomposes/jellyseer/docker-compose.yaml up -d
 
 
+
+# change ownership to media
+sudo chown -R root:media-server /home-server/media/
+sudo chown -R jellyfin /home-server/media/jellyfin/
+sudo chown -R qbit /home-server/media/qbittorrent/
+
+# change ownership to configs
+
+
+
+
+
+# crontab to save configs
+echo "0,15,30,45 * * * * cp -r /home-server/configs/ /mnt/home-server-save" | sudo crontab -u root -
